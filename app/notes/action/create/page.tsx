7edbb2f1fae2d8e48ad31fd996817409c6362505
Anton Bilyi <anthonybelyj@protonmail.com>
2025-08-20 13:03:34 +0300

    done metadata

---
+++ app/notes/action/create/page.tsx
@@ -0,0 +1,24 @@
+
+import css from "./CreateNote.module.css"
+
+
+
+
+
+
+
+const CreateNote = () => {
+
+
+    return (
+        <main className={css.main}>
+            <div className={css.container}>
+                <h1 className={css.title}>
+                   {/* <NoteForm/>  */}
+                </h1>
+            </div>
+        </main>
+    )
+}
+
+export default CreateNote;
